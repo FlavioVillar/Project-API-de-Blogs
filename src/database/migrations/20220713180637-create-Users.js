0d9d42fd794f053cef6079f5e4043295f4b0eebc
FlavioVillar <flavio_villar@hotmail.com>
2022-07-13 15:27:28 -0300

    feat: :card_file_box: created users migrations
    created user migration file

---
+++ src/database/migrations/20220713180637-create-Users.js
@@ -0,0 +1,48 @@
+'use strict';
+
+module.exports = {
+   /**
+   * @param {import('sequelize').queryInterface } queryInterface 
+   * @param {import('sequelize').Sequelize} Sequelize 
+   */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'display_name'
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
